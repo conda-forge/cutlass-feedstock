c36ff5eb3023d44d2a158a52fe1eca1f6d21fd23
Leo Fang <leof@nvidia.com>
2024-05-13 10:33:26 -0400

    don't build shared libraries

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,13 +15,12 @@ ln -s $BUILD_PREFIX/bin/x86_64-conda-linux-gnu-g++ $BUILD_PREFIX/bin/g++
 cmake \
     ${CMAKE_ARGS} \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_LIBRARY_OUTPUT_DIRECTORY="lib" \
     -DCUTLASS_ENABLE_F16C=ON \
     -DCUTLASS_ENABLE_CUDNN=ON \
     -DCUTLASS_ENABLE_CUBLAS=ON \
     -DCUTLASS_REVISION=${PKG_VERSION} \
     -DCUTLASS_ENABLE_TOOLS=ON \
-    -DCUTLASS_ENABLE_LIBRARY=ON \
+    -DCUTLASS_ENABLE_LIBRARY=OFF \
     -DCUTLASS_ENABLE_PROFILER=OFF \
     -DCUTLASS_ENABLE_TESTS=OFF \
     -DCUTLASS_ENABLE_EXAMPLES=OFF \
@@ -32,6 +31,3 @@ make -j$CPU_COUNT
 
 # install
 make install
-
-# remove static library
-rm -f $PREFIX/lib/libcutlass.a
